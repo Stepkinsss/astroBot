c3317f05ec5dde8690af56887ce01daefe9d5002
Stepkinsss <stepan0stepan@gmail.com>
2025-04-13 22:04:04 +0300

    Update city.py

--- city.py
+++ city.py
@@ -1,13 +1,17 @@
 import random
-
 import requests
 from googletrans import Translator
 import pandas as pd
 import datetime
 import ephem
+from datetime import datetime
+from skyfield.data import hipparcos
+from skyfield.api import load, Topos, Star
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+import pytz
 
 df = pd.read_csv('worldcities.csv')
-
 
 def translate_text(text, src_lang='auto', dest_lang='en'):
     translator = Translator()
@@ -99,3 +103,49 @@ def get_token():
     with open('token.txt', encoding='utf-8') as f:
         lines = [line.strip() for line in f]
     return lines[0]
+
+hip_to_name = {
+    32349: "Сириус", 71683: "Альфа-Центавра", 91262: "Вега", 113368: "Фомальгаут",
+    24608: "Процион", 37279: "Бетельгейзе", 24436: "Капелла", 53910: "Поллукс",
+    87833: "Денеб", 69673: "Арктур", 11767: "Альдебаран", 97649: "Альтаир",
+    21421: "Беллатрикс", 7588: "Ахернар", 677: "Хамаль", 65474: "Регула",
+    17651: "Ригель", 82273: "Спика", 59774: "Кастор",
+}
+
+def get_coordinates(city_name):
+    geolocator = Nominatim(user_agent="astro_star_viewer")
+    location = geolocator.geocode(city_name)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None
+#местное время для точности определения звёзд
+def get_local_time(lat, lon):
+    tf = TimezoneFinder()
+    tz_name = tf.timezone_at(lat=lat, lng=lon)
+    if tz_name:
+        local_tz = pytz.timezone(tz_name)
+        return datetime.now(local_tz)
+    else:
+        return None
+
+def get_visible_stars(lat, lon, max_objects=5):
+    local_time = get_local_time(lat, lon)
+    if not local_time:
+        return []
+    t = ts.from_datetime(local_time)
+    observer = earth + Topos(latitude_degrees=lat, longitude_degrees=lon)
+
+    visible = []
+    for hip_id, star in stars_df.iterrows():
+        ra_hours = star['ra_degrees'] / 15.0
+        dec_deg = star['dec_degrees']
+        mag = star['magnitude']
+        sky_star = Star(ra_hours=ra_hours, dec_degrees=dec_deg)
+        alt, az, _ = observer.at(t).observe(sky_star).apparent().altaz()
+        if alt.degrees > 0:
+            name = hip_to_name.get(hip_id, f"HIP {hip_id}")
+            visible.append((name, mag, alt.degrees))
+    visible.sort(key=lambda x: (x[1], -x[2]))
+    return visible[:max_objects]
+    
